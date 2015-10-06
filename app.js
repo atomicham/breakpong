51de51b48c02fe3aa78d514427878b09e2f8a441
Tyler Coles <shidobu@gmail.com>
2015-10-05 20:31:42 -0400

    Added basic angular setup.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-﻿console.log('Hello world');
+﻿// modules
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// server configuration
+var port = process.env.port || 1337;
+
+// define static route for static content directory.
+app.use('/static', express.static('public_html/static'));
+
+// define routes
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public_html/main.html');
+});
+
+http.listen(port, function () {
+	console.log('HTTP server listening on *:' + port);
+});
--- public_html/static/js/main.js
+++ public_html/static/js/main.js
@@ -1,1 +1,9 @@
-﻿
+﻿var pongBreakApp = angular.module('pongBreakApp', []);
+
+pongBreakApp.controller('pongBreakMasterController', function pongBreakMasterController() {
+
+});
+
+pongBreakApp.controller('pongBreakLobbyController', function pongBreakLobbyController() {
+
+});
