43380d70363179fdb7e5a3d21eb39bd611368a97
Tyler Coles <shidobu@gmail.com>
2015-10-16 13:54:45 -0400

    Added Google OAuth2 authentication.

--- app.js
+++ app.js
@@ -1,20 +1,73 @@
-﻿// modules
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var chat = require('./modules/chat.js')(http);
-var pongStorage = require('./modules/pong-storage.js');
-// server configuration
+﻿// modules.
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	passport = require('passport'),
+	ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
+	chat = require('./modules/chat.js')(http),
+	pongStorage = require('./modules/pong-storage.js')
+GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
+// configuration.
 var port = process.env.port || 1337;
+var host = process.env.hostaddr || '127.0.0.1';
 
-// define static route for static content directory.
+// setup express.
 app.use('/static', express.static('public_html/static'));
+app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// define routes
-app.get('/', function (req, res) {
+// passport setup.
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+	clientID: process.env.GOOGLE_CLIENT_ID,
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	callbackURL: "http://" + host + ":" + port + "/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+	process.nextTick(function () {
+		return done(null, profile);
+	});
+}
+));
+
+// passport auth routes
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+	// Successful authentication, redirect home.
+	res.redirect('/');
+});
+
+// define routes.
+app.get('/', ensureLoggedIn('/login'), function (req, res) {
 	res.sendFile(__dirname + '/public_html/main.html');
 });
 
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + '/public_html/login.html');
+});
+
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// start server.
 http.listen(port, function () {
 	console.log('HTTP server listening on *:' + port);
 });
