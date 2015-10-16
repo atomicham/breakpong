bb7e021eb767027611306338248c5c75eae19430
Tyler Coles <shidobu@gmail.com>
2015-10-15 21:30:29 -0400

    Small updates to storage

--- modules/pong-storage.js
+++ modules/pong-storage.js
@@ -1,18 +1,19 @@
 ﻿
 var azure = require('azure-storage');
+var entityGen = azure.TableUtilities.entityGenerator;
 
 // use seperate table for development
 // create the user table
 var tableSvc = azure.createTableService();
 tableSvc.createTableIfNotExists('users', function (error, result, response) {
 	if (!error) {
-
+		console.log('Users table exists.')
 	}
 });
 
 tableSvc.createTableIfNotExists('games', function (error, result, response) {
 	if (!error) {
-
+		console.log('Games table exists.')
 	}
 });
 
