5de9fee47d8711f8b73e565e0b28ad2e9b51be9b
Tyler Coles <shidobu@gmail.com>
2015-10-16 20:42:41 -0400

    OAuth to Azure Storage

--- modules/pong-storage.js
+++ modules/pong-storage.js
@@ -1,17 +1,43 @@
 ﻿module.exports = (function () {
 	// require modules
 	var azure = require('azure-storage');
-
+	var entityGen = azure.TableUtilities.entityGenerator;
+	
 	// module ref.
 	var m = {};
 	
+	var User = function () {
+		this.name = null;
+	};
+	
+	User.MapFromEntity = function $User$MapFromEntity(entity) {
+		var user = new User();
+		user.name = entity.RowKey._;
+		
+		return user;
+	};
+	
+	User.MapToEntity = function $User$MapToEntity(user) {
+		var entity = {
+			PartitionKey: entityGen.String('registered'),
+			RowKey: entityGen.String(user.name)
+		};
+		return entity;
+	};
+	
+	var Game = function () {
+		this.id = "0000000";
+	};
+	
+	// members
+	var tableSvc = null;
+	
 	// initialize method
 	m.initialize = function $init() {
-		var entityGen = azure.TableUtilities.entityGenerator;
 		
 		// use seperate table for development
 		// create the user table
-		var tableSvc = azure.createTableService();
+		tableSvc = azure.createTableService();
 		tableSvc.createTableIfNotExists('users', function (error, result, response) {
 			if (!error) {
 				console.log('Users table exists.')
@@ -24,19 +50,59 @@
 			}
 		});
 	};
-
+	
 	// get user
 	// add user
 	// update user
 	m.User = {
 		//findOrCreate		
 		findOrCreate: function $User$findOrCreate(profile, callback) {
-			debugger;
-			// get user from db
-			var u = profile;
-			var e = null;
-			// if error, 
-			callback(e, u);
+			var query = new azure.TableQuery();
+			
+			if (!!profile.googleId) {
+				query.where('GoogleID eq ?', profile.googleId);
+			} else if (!!profile.githubId) {
+				query.where('GitHubID eq ?', profile.githubId);
+			}
+			
+			var err = null;
+			
+			tableSvc.queryEntities('users', query, null, function (error, result, response) {
+				if (!error) {
+					// result contains the entities
+					// get user from db
+					if (result.entries.length > 0) {
+						// return existing user
+						profile = User.MapFromEntity(result.entries[0]);
+						
+						callback(err, profile);
+					} else {
+						// create user (?)
+						var user = {
+							PartitionKey: entityGen.String('registered'),
+							RowKey: entityGen.String('Anonymous')
+						};
+						
+						if (!!profile.googleId) {
+							user.GoogleID = entityGen.String(profile.googleId);
+						}
+						
+						tableSvc.insertEntity('users', user, { echoContent: true }, function (error, result, response) {
+							if (!error) {
+								var user = User.MapFromEntity(result);
+								callback(err, user);
+							} else {
+								callback(error, profile);
+							}
+						});
+					}
+					
+					
+					// if error, 
+				} else {
+					callback(error, profile);
+				}
+			});
 		}
 	};
 	
@@ -48,8 +114,8 @@
 
 		}
 	};
-
-
+	
+	
 	// return complete module
 	return m;
 }());
