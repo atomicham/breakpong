ed390ad8757bc7c118a98be55b7063d548d2e834
Tyler Coles <tylerevancoles@gmail.com>
2015-10-18 12:16:40 -0400

    separated chat into a separate module

---
+++ static/app/chat.js
@@ -0,0 +1,38 @@
+﻿angular.module('chatModule', [])
+	.controller('chatController', ['$scope', function chatController($scope) {
+		var socket = io(window.location.origin + '/chat');
+		$scope.chat = {
+			message: '',
+			messages: [],
+			sendMessage: function () {
+				var value = this.message;
+				if (!!value && !!value.trim()) {
+					socket.emit('client msg', value);
+					this.message = '';
+				}
+				return false;
+			},
+			messageKeyUp: function (e) {
+				if (e.keyCode == 13) {
+					this.sendMessage();
+				}
+			},
+			addMessage: function (msg) {
+				$scope.$apply(function () {
+					$scope.chat.messages.push(msg);
+				});
+			}
+		};
+
+		socket.on('serv msg', function (msg) {
+			$scope.chat.addMessage(msg);
+		});
+
+		socket.on('serv usr conn', function (msg) {
+			$scope.chat.addMessage(msg);
+		});
+
+		socket.on('serv usr disc', function (msg) {
+			$scope.chat.addMessage(msg);
+		});
+	}]);
--- static/app/main.js
+++ static/app/main.js
@@ -1,4 +1,4 @@
-﻿var pongBreakApp = angular.module('pongBreakApp', ['ngRoute']);
+﻿var pongBreakApp = angular.module('pongBreakApp', ['ngRoute', 'chatModule']);
 
 pongBreakApp.directive('script', function ($parse, $rootScope, $compile) {
 	return {
@@ -52,42 +52,4 @@ pongBreakApp.controller('gameController', ['$scope', function pongBreakLobbyCont
 
 pongBreakApp.controller('accountController', ['$scope', function pongBreakLobbyController($scope) {
 	$scope.toggleChat(false);
-}]);
-
-pongBreakApp.controller('chatController', ['$scope', function chatController($scope) {
-	var socket = io(window.location.origin + '/chat');
-	$scope.chat = {
-		message: '',
-		messages: [],
-		sendMessage: function () {
-			var value = this.message;
-			if (!!value && !!value.trim()) {
-				socket.emit('client msg', value);
-				this.message = '';
-			}
-			return false;
-		},
-		messageKeyUp: function (e) {
-			if (e.keyCode == 13) {
-				this.sendMessage();
-			}
-		},
-		addMessage: function (msg) {
-			$scope.$apply(function () {
-				$scope.chat.messages.push(msg);
-			});
-		}
-	};
-
-	socket.on('serv msg', function (msg) {
-		$scope.chat.addMessage(msg);
-	});
-
-	socket.on('serv usr conn', function (msg) {
-		$scope.chat.addMessage(msg);
-	});
-
-	socket.on('serv usr disc', function (msg) {
-		$scope.chat.addMessage(msg);
-	});
 }]);
