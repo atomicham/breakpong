3d5068c969b066f9edfd0007dbcb26ce9a81097d
Tyler Coles <tylerevancoles@gmail.com>
2015-10-18 11:57:10 -0400

    refactored chat window to use angular.

--- static/app/main.js
+++ static/app/main.js
@@ -54,6 +54,40 @@ pongBreakApp.controller('accountController', ['$scope', function pongBreakLobbyC
 	$scope.toggleChat(false);
 }]);
 
-pongBreakApp.controller('chatController', function chatController() {
+pongBreakApp.controller('chatController', ['$scope', function chatController($scope) {
+	var socket = io(window.location.origin + '/chat');
+	$scope.chat = {
+		message: '',
+		messages: [],
+		sendMessage: function () {
+			var value = this.message;
+			if (!!value && !!value.trim()) {
+				socket.emit('client msg', value);
+				this.message = '';
+			}
+			return false;
+		},
+		messageKeyUp: function (e) {
+			if (e.keyCode == 13) {
+				this.sendMessage();
+			}
+		},
+		addMessage: function (msg) {
+			$scope.$apply(function () {
+				$scope.chat.messages.push(msg);
+			});
+		}
+	};
 
-});
+	socket.on('serv msg', function (msg) {
+		$scope.chat.addMessage(msg);
+	});
+
+	socket.on('serv usr conn', function (msg) {
+		$scope.chat.addMessage(msg);
+	});
+
+	socket.on('serv usr disc', function (msg) {
+		$scope.chat.addMessage(msg);
+	});
+}]);
